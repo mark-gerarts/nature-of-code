f425e6858caf12512c46bc243f053360dab5a890
Mark Gerarts <mark.gerarts@gmail.com>
2018-05-01 15:48:06 +0200

    Add walker that tends to the right

---
+++ Introduction/Example I.3: Walker that tends to move to the right/README.md
@@ -0,0 +1,3 @@
+# Example I.3: Walker that tends to move to the right
+
+![Example I.3: Walker that tends to move to the right](https://raw.githubusercontent.com/mark-gerarts/nature-of-code/master/screenshots/Example%20I.3%3A%20Walker%20that%20tends%20to%20move%20to%20the%20right.gif)
---
+++ Introduction/Example I.3: Walker that tends to move to the right/sketch.lisp
@@ -0,0 +1,50 @@
+(defpackage :sketch (:use :cl :trivial-gamekit))
+(in-package :sketch)
+
+(defvar *width* 800)
+(defvar *height* 600)
+(defvar *black* (vec4 0 0 0 1))
+
+(defun empty-array ()
+  (make-array 1 :adjustable t :fill-pointer 0))
+
+(defun mid ()
+  (vec2 (/ *width* 2) (/ *height* 2)))
+
+;; We'll represent a walker without using CLOS to minimize memory usage. This
+;; because we have to remember previous states.
+(defparameter *previous-states* (empty-array))
+(defparameter *walker* (mid))
+
+(defun draw-walker (walker)
+  (draw-rect walker 1 1 :fill-paint *black*))
+
+(defun step-walker (walker)
+  (let ((r (random 1.0))
+        (x (x *walker*))
+        (y (y *walker*)))
+    ;; Store the old state and update the walker.
+    (vector-push-extend *walker* *previous-states*)
+    (cond
+      ((< r 0.4) (incf x))
+      ((< r 0.6) (decf x))
+      ((< r 0.8) (incf y))
+      (t (decf y)))
+    (setf *walker* (vec2 x y))))
+
+(defgame sketch () ()
+  (:viewport-width *width*)
+  (:viewport-height *height*)
+  (:viewport-title "Random walker"))
+
+(defmethod post-initialize ((this sketch))
+  (setf *walker* (mid))
+  (setf *previous-states* (empty-array)))
+
+(defmethod draw ((this sketch))
+  (loop for w across *previous-states* do (draw-walker w)))
+
+(defmethod act ((this sketch))
+  (step-walker *walker*))
+
+(start 'sketch)
